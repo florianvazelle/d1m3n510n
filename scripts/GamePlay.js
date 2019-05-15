4b7940556ea170c96b5f9587b83fb76c77750546
florianvazelle <ponythugflorian@gmail.com>
2019-05-15 21:00:48 +0200

    Remove warning itch.io

--- scripts/GamePlay.js
+++ scripts/GamePlay.js
@@ -177,3 +177,11 @@ var gamePlayState = new Phaser.Class({
 });
 
 myGame.scenes.push(gamePlayState);
+
+function spawnEnemy() {
+  return (Math.random() > 0.99);
+}
+
+function updateText(score, life) {
+  return (score == 0) ? 'Arrow keys to move.\nLeft click to shoot.\nWe must kill the enemies in their dimensions.' : 'Score : ' + score + '\nLife : ' + life;
+}
--- scripts/utils.js
+++
@@ -1,7 +0,0 @@
-function spawnEnemy() {
-  return (Math.random() > 0.99);
-}
-
-function updateText(score, life) {
-  return (score == 0) ? 'Arrow keys to move.\nLeft click to shoot.\nWe must kill the enemies in their dimensions.' : 'Score : ' + score + '\nLife : ' + life;
-}
